00e18ca00b25a38fc15cd3b5dde4cb2329808cb5
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-11-20 14:04:05 +0000

    Add framework projection for functions
    Deals with dependency injection and function initialisation

---
+++ src/SFA.DAS.Campaign.Functions.Framework/Infrastructure/InjectConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs.Host.Config;
+using SFA.DAS.Campaign.Functions.Framework.Attributes;
+
+namespace SFA.DAS.Campaign.Functions.Framework.Infrastructure
+{
+    internal class InjectConfiguration : IExtensionConfigProvider
+    {
+        public readonly InjectBindingProvider InjectBindingProvider;
+
+        public InjectConfiguration(InjectBindingProvider injectBindingProvider) =>
+            InjectBindingProvider = injectBindingProvider;
+
+        public void Initialize(ExtensionConfigContext context) => context
+            .AddBindingRule<InjectAttribute>()
+            .Bind(InjectBindingProvider);
+    }
+}
---
+++ src/SFA.DAS.Campaign.Functions.Framework/Infrastructure/ScopeCleanupFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace SFA.DAS.Campaign.Functions.Framework.Infrastructure
+{
+    internal class ScopeCleanupFilter : IFunctionInvocationFilter, IFunctionExceptionFilter
+    {
+        private readonly ServiceProviderHolder _scopeHolder;
+
+        public ScopeCleanupFilter(ServiceProviderHolder scopeHolder) =>
+            _scopeHolder = scopeHolder;
+
+        public Task OnExceptionAsync(FunctionExceptionContext exceptionContext, CancellationToken cancellationToken)
+        {
+            _scopeHolder.RemoveScope(exceptionContext.FunctionInstanceId);
+            return Task.CompletedTask;
+        }
+
+        public Task OnExecutedAsync(FunctionExecutedContext executedContext, CancellationToken cancellationToken)
+        {
+            _scopeHolder.RemoveScope(executedContext.FunctionInstanceId);
+            return Task.CompletedTask;
+        }
+
+        public Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken) =>
+            Task.CompletedTask;
+    }
+}
