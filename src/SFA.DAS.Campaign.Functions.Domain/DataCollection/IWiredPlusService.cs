57d8bbddd84af0fe4bbe8003fd6d8ad861864e30
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-11-27 10:12:50 +0000

    Add interface for wiredplus service

---
+++ src/SFA.DAS.Campaign.Functions.Domain/DataCollection/IWiredPlusService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.Campaign.Functions.Models.DataCollection;
+
+namespace SFA.DAS.Campaign.Functions.Domain.DataCollection
+{
+    public interface IWiredPlusService
+    {
+        Task CreateUser(UserData user);
+    }
+}
