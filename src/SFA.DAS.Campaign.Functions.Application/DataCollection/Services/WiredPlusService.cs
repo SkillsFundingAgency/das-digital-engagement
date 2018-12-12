e01f9f83d4280a39be0dace4e6b5c904f4e8f390
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-12-12 08:00:18 +0000

    Change WiredPlusService to use WiredPlusUser
    Added WiredPlusUser model with mapper from UserData. This is then used
    for sending requests to wired plus

--- src/SFA.DAS.Campaign.Functions.Application/DataCollection/Services/WiredPlusService.cs
+++ src/SFA.DAS.Campaign.Functions.Application/DataCollection/Services/WiredPlusService.cs
@@ -63,12 +63,14 @@ namespace SFA.DAS.Campaign.Functions.Application.DataCollection.Services
 
         private static Dictionary<string, string> UserDataToDictionary(UserData user)
         {
-            return user.GetType()
+            var wiredPlusUser = new WiredPlusUser().MapFromUserData(user);
+
+            return wiredPlusUser.GetType()
                 .GetProperties(BindingFlags.Instance | BindingFlags.Public)
                 .Where(key => key.CustomAttributes.ToList().Any())
                 .ToDictionary(key => 
                         key.CustomAttributes.ToList()[0].ConstructorArguments[0].Value.ToString(),
-                    value => value.GetValue(user) == null ? "" : value.GetValue(user).ToString());
+                    value => value.GetValue(wiredPlusUser) == null ? "" : value.GetValue(wiredPlusUser).ToString());
         }
 
         public async Task<bool> UserExists(string email)
