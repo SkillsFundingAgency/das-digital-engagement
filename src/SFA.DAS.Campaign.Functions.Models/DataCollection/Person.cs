9acb31c3719cbac07ef53720499e3e1a059b42cf
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-12-17 08:25:26 +0000

    Update Person Model
    Updated the person model so that the JSON properties are only mapped to
    if there are values supplied.

--- src/SFA.DAS.Campaign.Functions.Models/DataCollection/Person.cs
+++ src/SFA.DAS.Campaign.Functions.Models/DataCollection/Person.cs
@@ -5,31 +5,32 @@ namespace SFA.DAS.Campaign.Functions.Models.DataCollection
 {
     public class Person
     {
-        [JsonProperty("firstName")]
+        [JsonProperty("firstName",NullValueHandling = NullValueHandling.Ignore)]
         public string FirstName { get; internal set; }
-        [JsonProperty("lastName")]
+        [JsonProperty("lastName", NullValueHandling = NullValueHandling.Ignore)]
         public string LastName { get; internal set; }
-        [JsonProperty("enrolled")]
+        [JsonProperty("enrolled", NullValueHandling = NullValueHandling.Ignore)]
         public string Enrolled { get; internal set; }
         [JsonProperty("Consent")]
         public PersonConsent Consent { get; internal set; }
-        [JsonProperty("Cookie")]
+        [JsonProperty("Cookie", NullValueHandling = NullValueHandling.Ignore)]
         public PersonCookie Cookie { get; internal set; }
-        [JsonProperty("Route")]
+        [JsonProperty("Route", NullValueHandling = NullValueHandling.Ignore)]
         public PersonRoute Route { get; internal set; }
         [JsonProperty("ContactDetail")]
         public PersonContactDetail ContactDetail { get; internal set; }
 
         public Person MapFromUserData(UserData user)
         {
+            var isCreateUser = IsCreateUser(user);
             return new Person
             {
                 FirstName = user.FirstName,
                 LastName = user.LastName,
-                Enrolled = DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"),
+                Enrolled = isCreateUser ? DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss") : null,
                 ContactDetail = new PersonContactDetail
                 {
-                    Captured = DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"),
+                    Captured = isCreateUser ? DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss") : null,
                     EmailAddress = user.Email,
                     EmailVerificationCompleted = null,
                 },
@@ -38,17 +39,22 @@ namespace SFA.DAS.Campaign.Functions.Models.DataCollection
                     GdprConsentDeclared = DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"),
                     GdprConsentGiven = user.Consent
                 },
-                Cookie = new PersonCookie
+                Cookie = isCreateUser ? new PersonCookie
                 {
                     Captured = DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"),
                     CookieIdentifier = user.CookieId
-                },
-                Route = new PersonRoute
+                } : null,
+                Route = isCreateUser ? new PersonRoute
                 {
                     Captured = DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"),
                     RouteIdentifier = user.RouteId
-                }
+                } : null
             };
+        }
+
+        private bool IsCreateUser(UserData user)
+        {
+            return !string.IsNullOrEmpty(user.FirstName) && !string.IsNullOrEmpty(user.LastName);
         }
     }
 }
--- src/SFA.DAS.Campaign.Functions.Models/DataCollection/PersonContactDetail.cs
+++ src/SFA.DAS.Campaign.Functions.Models/DataCollection/PersonContactDetail.cs
@@ -5,11 +5,11 @@ namespace SFA.DAS.Campaign.Functions.Models.DataCollection
 {
     public class PersonContactDetail
     {
-        [JsonProperty("Captured")]
+        [JsonProperty("Captured", NullValueHandling = NullValueHandling.Ignore)]
         public string Captured { get; set; }
         [JsonProperty("emailAddress")]
         public string EmailAddress { get; set; }
-        [JsonProperty("emailVerificationCompletion")]
+        [JsonProperty("emailVerificationCompletion", NullValueHandling = NullValueHandling.Ignore)]
         public string EmailVerificationCompleted { get; set; }
     }
 }
