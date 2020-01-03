0d0555d2178ccbeba5179c9231b48fc187eeae33
Matt Derry <matt.derry@digital.education.gov.uk>
2020-01-03 11:17:59 +0000

    Adds Marketo Lead Api Client

---
+++ src/SFA.DAS.Campaign.Functions.Application/Infrastructure/Interfaces/Marketo/IMarketoLeadClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+using SFA.DAS.Campaign.Functions.Models.Marketo;
+
+namespace SFA.DAS.Campaign.Functions.Application.Services
+{
+    public interface IMarketoLeadClient
+    {
+        [Get("/rest/v1/lead/{id}.json")]
+        ResponseOfLead Get(string id);
+        [Post("/rest/v1/leads/push.json")]
+        ResponseOfPushLeadToMarketo PushLead(PushLeadToMarketoRequest pushLead);
+        [Post("/rest/v1/leads/{id}/associate.json?cookie={cookieId}")]
+        ResponseWithoutResult AssociateLead(string id, string cookieId);
+    }
+}
