abc89bd00858a3dbb98fb7c1512010caf35df3b9
Matt Derry <matt.derry@digital.education.gov.uk>
2020-06-01 12:29:14 +0100

    Adds import function infrastructure

---
+++ src/DAS.DigitalEngagement.Functions.ImportCsv/startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using DAS.DigitalEngagement.Framework.Infrastructure.Configuration;
+using DAS.DigitalEngagement.Functions.Import;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using SFA.DAS.Configuration.AzureTableStorage;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace DAS.DigitalEngagement.Functions.Import
+{
+    public class Startup : FunctionsStartup
+    {
+         
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var sp = builder.Services.BuildServiceProvider();
+
+            var configuration = sp.GetService<IConfiguration>();
+
+            var nLogConfiguration = new NLogConfiguration();
+
+            var tempConfig = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables()
+                .AddJsonFile("local.settings.json", true).Build();
+
+            builder.Services.AddLogging((options) =>
+            {
+                options.SetMinimumLevel(LogLevel.Trace);
+                options.SetMinimumLevel(LogLevel.Trace);
+                options.AddNLog(new NLogProviderOptions
+                {
+                    CaptureMessageTemplates = true,
+                    CaptureMessageProperties = true
+                });
+                options.AddConsole();
+
+                nLogConfiguration.ConfigureNLog(tempConfig);
+            });
+
+            var config = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .AddEnvironmentVariables()
+                .AddAzureTableStorage(options =>
+                {
+                    options.ConfigurationKeys = new[] { tempConfig.GetValue<string>("AppName") };
+                    options.EnvironmentNameEnvironmentVariableName = "EnvironmentName";
+                    options.StorageConnectionStringEnvironmentVariableName = "ConfigurationStorageConnectionString";
+                })
+                .Build();
+            
+            builder.Services.AddOptions();
+            builder.Services.Configure<ConnectionStrings>(config.GetSection("ConnectionStrings"));
+
+
+        }
+    }
+}
---
+++ src/DAS.DigitalEngagement.Models/Infrastructure/ConnectionStrings.cs
@@ -0,0 +1,4 @@
+public class ConnectionStrings
+{
+    public string Sql { get; set; }
+}
