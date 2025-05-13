ac2f0cf11431e2af92b130033612889a35b3ab29
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-05-13 09:13:49 +0100

    P2-2374-Marketo stage 5

---
+++ src/DAS.DigitalEngagement.Application/DataCollection/Validators/CreatedAccountTaskListCompleteValidator.cs
@@ -0,0 +1,15 @@
+﻿using DAS.DigitalEngagement.Domain.DataCollection;
+using SFA.DAS.EmployerAccounts.Messages.Events;
+
+namespace DAS.DigitalEngagement.Application.DataCollection.Validators
+{
+    public class CreatedAccountTaskListCompleteValidator : ICreatedAccountTaskListCompleteValidator
+    {
+        public bool Validate(CreatedAccountTaskListCompleteEvent userData)
+        {
+            if (userData == null) return false;
+
+            return !string.IsNullOrWhiteSpace(userData.UserRef.ToString());
+        }
+    }
+}
